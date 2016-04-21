718b83f937c6b975e05c5053c9a7e336806f6643
Alexander Kuzmin <alex.kuzminx@gmail.com>
2016-04-21 23:22:18 +0100

    Update magic_ball.rb

--- magic_ball.rb
+++ magic_ball.rb
@@ -1,40 +1,42 @@
-require_relative 'win_cp_fix' #фиксим кодировочку :)
+require_relative 'win_cp_fix' #fixing Cyrillic letter encoding problem on Windows
 
-#задаем массив приветствий
+#Defining greetings array
 
 greetings = [
-		'Hello, my dear friend! What is your name',
-		'And who is asking?',
-		'Greetings, human! What should i call you?']
+  'Hello, my dear friend! What is your name',
+  'Hello! And who is asking?',
+  'Greetings, human! What should i call you?'
+]
 
-#Задаем массив ответов и перечисляем его элементы
+#Defining possible answers
 
 answers = [
-    #Positive answers
-		'It is certain',
-		'It is decidedly so',
-		'Without a doubt',
-		'Yes, definitely',
-		'You may rely on it',
-		'As I see it, yes',
-		'Most likely',
-		'Outlook good',
-		'Yes',
-		'Signs point to yes',
-		#Uncertain answers
-		'Reply hazy, try again',
-		'Ask again later',
-		'Better not tell you now',
-		'Cannot predict now',
-		'Concentrate and ask again',
-		#Negative answers
-		'Don\'t count on it',
-		'My reply is no',
-		'My sources say no',
-		'Outlook not so good',
-		'Very doubtful']
+  #Positive answers
+  'It is certain',
+  'It is decidedly so',
+  'Without a doubt',
+  'Yes, definitely',
+  'You may rely on it',
+  'As I see it, yes',
+  'Most likely',
+  'Outlook good',
+  'Yes',
+  'Signs point to yes',
+  #Uncertain answers
+  'Reply hazy, try again',
+  'Ask again later',
+  'Better not tell you now',
+  'Cannot predict now',
+  'Concentrate and ask again',
+  #Negative answers
+  'Don\'t count on it',
+  'My reply is no',
+  'My sources say no',
+  'Outlook not so good',
+  'Very doubtful'
+]
 
-#выводим приветствие, спрашиваем имя и потом случайный элемент массива
+#printing a random greeting, asking name and then printing a random answer
 
 puts greetings.sample
 name = gets.chomp.encode('utf-8')
